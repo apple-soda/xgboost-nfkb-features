8f7b2d29389ad0df3cda3247af2ce5b5fd37f89f
kekpy <kekpyds@gmail.com>
2022-04-02 21:29:55 -0700

    ip

--- core/dprocessing.py
+++ core/dprocessing.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 
-def process(features, time_steps, dataset):
+def process_features(features, time_steps, dataset):
     df = pd.DataFrame()
     for feature, time_step in zip(features, time_steps):
         if time_step is not None:
@@ -12,3 +12,17 @@ def process(features, time_steps, dataset):
         df = pd.concat([df, data_section], axis=1)
     
     return df
+
+def partition_features(features, time_steps, feature_list, bound, dataset):
+    present_features = []
+    partitioned_list = feature_list[:int(len(feature_list) * bound)]
+    partitioned_list = [item for sublist in partitioned_list for item in sublist]
+    for feature, time_step in zip(features, time_steps):
+        if time_step is not None:
+            for i in range(1, time_step):
+                if feature + '_' + str(i) in partitioned_list:
+                    present_features.append(feature + '_' + str(i))
+        else:
+            present_features.append(feature)
+           
+    return dataset[present_features]
